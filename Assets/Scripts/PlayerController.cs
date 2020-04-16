71c27548288247f4736f943dc5ca9bb63ae21d66
EmilJK <emjoka18@gmail.com>
2020-04-16 16:21:03 +0200

    1st commit
    Project startup, gravity on player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    #region Player Info
+    [Header("Player Values")]
+    public float playerGravity = 0.2f;
+    public float maxVelocity = 2f;
+
+    #endregion
+
+    public Rigidbody2D rb;
+    void awake()
+    {
+        rb.gravityScale = playerGravity;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //When screen pressed set the velocity of snake 0
+        {
+            rb.velocity = Vector2.zero;
+        }
+        rb.AddForce(-rb.velocity * maxVelocity); //Damps the velocity of the player
+    }
+}
