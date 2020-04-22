d0f9bc4d43a103a707724e9e584f2d320eb46687
EmilJK <emjoka18@gmail.com>
2020-04-22 12:00:09 +0200

    21/04/2020 Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,22 +8,36 @@ public class PlayerController : MonoBehaviour
     #region Player Info
     [Header("Player Values")]
     public float playerGravity = 0.2f;
-    public float maxVelocity = 2f;
+    public float velocityDamp = 2f;
+    public float tiltSpeed = 1f;
+
+    float dirX;
 
     #endregion
 
     public Rigidbody2D rb;
     void awake()
     {
+        rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = playerGravity;
     }
 
     void Update()
     {
+        //dirX = Input.acceleration.x * tiltSpeed;
+        //transform.position = new Vector2(transform.position.x, transform.position.y);
+
+        transform.Translate(Input.acceleration.x * tiltSpeed, 0,0);
+
         if (Input.GetMouseButtonDown(0)) //When screen pressed set the velocity of snake 0
         {
             rb.velocity = Vector2.zero;
         }
-        rb.AddForce(-rb.velocity * maxVelocity); //Damps the velocity of the player
+        rb.AddForce(-rb.velocity * velocityDamp); //Damps the velocity of the player
+    }
+
+    private void FixedUpdate()
+    {
+        //rb.velocity = new Vector2(dirX, rb.velocity.y);
     }
 }
